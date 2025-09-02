322479e9b4e20ce4de271f9cc10462a220d27418
IT24101569 <it24101569@my.sliit.lk>
2025-09-02 14:18:07 +0530

    Lab06

---
+++ Lab Sheet 06/IT24101569.R
@@ -0,0 +1,23 @@
+setwd("C:\\Users\\IT24101569\\Desktop\\Lab 05")
+
+#Exercise
+#01
+
+#i
+#Binomial Distribution
+#Here, Random veriable X has Binomial Distribution with n = 50 and p = 0.85 
+
+#ii
+pbinom(47,50,0.85,lower.tail = FALSE)
+
+#02
+
+#i
+#Customer calls per hour
+
+#ii
+#Poisson Distribution
+#Here, Random veriable X has Poisson Distribution with lamda = 12 
+
+#iii
+dpois(15,12)
